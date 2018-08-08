b93586016d5410b5bbb59f98358ce642ab4e6e77
ChinksofLight <ounane.farouk@gmail.com>
2018-08-08 20:30:47 +0100

    Update mandelbrot.cpp

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -12,11 +12,11 @@ int value (int x, int y)  {
     complex<float> point((float)x/width-1.5, (float)y/height-0.5);
     complex<float> z(0, 0);
     int nb_iter = 0;
-    while (abs (z) < 2 && nb_iter < 100) {
+    while (abs (z) < 2 && nb_iter <= 20) {
         z = z * z + point;
         nb_iter++;
     }
-    if (nb_iter <= 20)
+    if (nb_iter < 20)
        return (255*nb_iter)/20;
     else 
        return 0;
