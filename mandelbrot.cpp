2052398d67aeb85900311a8c1b12c699fda27648
OUNANE MOHAMED ELFAROUK <ounane.farouk@gmail.com>
2021-03-12 22:08:15 +0100

    Update mandelbrot.cpp

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -29,7 +29,7 @@ int main ()  {
     if (my_Image.is_open ()) {
         my_Image << "P3\n" << WIDTH << " " << HEIGHT << " 255\n";
         for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < height; j++)  {
+            for (int j = 0; j < HEIGHT; j++)  {
                 int val = value(i, j);
                 my_Image << val << ' ' << 0 << ' ' << 0 << "\n";
             }
